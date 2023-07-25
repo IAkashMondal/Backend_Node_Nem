31bbd1b0acfe935df56263b3b51218cfbd94af33
IAkashMondal <adas085322gmail.com>
2023-07-25 19:25:56 +0530

    rest api created

--- Day-8-Rest-Api/index.js
+++ Day-8-Rest-Api/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
-const Port = 5400 ;
+const fs = require("fs");
+const Port = 5400;
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.send("homepage");
 });
@@ -17,6 +19,22 @@ app.get("/api/users/:id", (req, res) => {
   }
   return res.json(userWithId);
 });
+app.post("/api/adduser", (req, res) => {
+  const data = req.body;
+  console.log(data, "data");
+  const newUserId = users.length + 1;
+  users.push({ ...data, id: newUserId });
+
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Failed to write to file." });
+    }
+    return res.json({data, message: "Data added successfully." });
+  });
+});
+app.patch("/api/edituser/:id", (req, res) => {});
+app.delete("/api/deleteuser/:id", (req, res) => {});
 
 app.listen(Port, () => {
   console.log(`Server Runing on...... at Port: ${Port}`);
